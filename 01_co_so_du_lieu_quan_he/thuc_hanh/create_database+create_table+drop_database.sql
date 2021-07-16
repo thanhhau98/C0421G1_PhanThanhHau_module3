2ceee7cc36897185f8a4e9880fde30efdbc1181d
ThanhHau <th010498@gmail.com>
2021-07-16 11:24:26 +0700

    bai tap 1

---
+++ 01_co_so_du_lieu_quan_he/thuc_hanh/create_database+create_table+drop_database.sql
@@ -0,0 +1,12 @@
+create database my_database ;
+
+use my_database;
+
+create table student (
+id int primary key ,
+`name` varchar(45) null,
+age int null,
+country varchar(45) null
+);
+
+drop database my_database ;
